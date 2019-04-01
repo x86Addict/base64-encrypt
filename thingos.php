c149662efaee0313d85fdc5148fc5d47a8ebbbb2
x86Addict <49136558+x86Addict@users.noreply.github.com>
2019-03-31 23:19:08 -0400

    Create thingos.php

---
+++ thingos.php
@@ -0,0 +1,21 @@
+$str = "String|UserID|OtherStuff";
+$reverseBase64 = strrev(base64_encode($str)); //this reversifies to be harder to read.
+$normalfy = base64_decode(strrev($reverseBase64)); //it's decoding reversed string not. reversing decoded string. my trademark normalifies it.
+
+if(isset($_POST['play'])){
+header("Location: application:<?php echo $reverseBase64;?>");
+}
+/*
+You can use this for passing URI parameters see below.
+*/
+?>
+<!--<a href="application:<?php echo $reverseBase64;?>">Launch program</a>-->
+<input type="submit" name="play" value="Launch program">
+<?php
+echo"This is for further protection if needed".$base64;
+echo"This is normal".$normalfy;
+
+//Something cool. can be used as a pleasant surprise.
+$bruh = "header('location: bruh.com');";
+echo eval(base64_decode($bruh)); //just base64 no variable even needed
+?>
